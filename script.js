c5f45762dc3c65f0b981bc6404ce8f614032ae03
Thano <ats47195@stud.hs-furtwangen.de>
2025-04-16 13:05:31 +0200

    js update

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let Spielfeld = ["","","","","","","","",""];
-let Spieler = "X";
-let Spielbeginn = true;
+let sekunden = 0;
+let timerLaeuft = null;
 
-const Felder = document.querySelectorAll('.Feld');
+function nameCheck() {
+    const name = document.getElementById("nameInput").value.trim();
+    if (name.length !== 0) {
+      starteTimer();
+    }
+  }
+
+function starteTimer() {
+  timerLaeuft = setInterval(() => {
+    sekunden++;
+    updateZeitAnzeige();
+  }, 1000);
+}
+
+function updateZeitAnzeige() {
+    const minuten = Math.floor(sekunden / 60);
+    const secs = sekunden % 60;
+    const timeString = `${minuten}:${secs < 10 ? `0` : ``}${secs}`;
+    document.getElementById("Zeitanzeige").innerText = timeString;
+}
+
+window.addEventListener("ButtonEventListener", () => {
+    starteTimer();
+})
+
