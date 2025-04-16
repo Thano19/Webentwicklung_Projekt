41572300023fcc6a068c81c529eb993e9254b1ed
Your Name <you@example.com>
2025-04-16 14:15:51 +0200

    game update

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-let Spielfeld = ["","","","","","","","",""];
-let Spieler = "X";
-let Spielbeginn = true;
+let sekunden = 0;
+let timerLaeuft = null;
+let spieler = "X";
 
-const Felder = document.querySelectorAll('.Feld');
+function nameCheck() {
+    const name = document.getElementById("nameInput").value.trim();
+    if (name.length !== 0) {
+        ingame("X");
+        starteTimer();
+    }
+  }
+
+function starteTimer() {
+  timerLaeuft = setInterval(() => {
+    sekunden++;
+    updateZeitAnzeige();
+  }, 1000);
+}
+
+function updateZeitAnzeige() {
+    const minuten = Math.floor(sekunden / 60);
+    const secs = sekunden % 60;
+    const timeString = `${minuten}:${secs < 10 ? `0` : ``}${secs}`;
+    document.getElementById("Zeitanzeige").innerText = timeString;
+}
+
+function ingame(spieler) {
+    const felder = document.querySelectorAll(".Feld");
+    felder.forEach((feld) => {
+        if (!feld.classList.contains("listener-hinzugefuegt")) {
+            feld.addEventListener("click", () => {
+                if (feld.textContent !== "") 
+                    return;
+                feld.textContent = spieler;
+                feld.classList.add("deaktiviert");
+            })
+            feld.classList.add("listener-hinzugefuegt");
+        }
+    })
+}
